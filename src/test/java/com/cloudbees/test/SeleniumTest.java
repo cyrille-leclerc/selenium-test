1da90203f6ca275525fb7f03ec7984f027a492e1
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2017-01-18 12:08:12 +0100

    initial commit

---
+++ src/test/java/com/cloudbees/test/SeleniumTest.java
@@ -0,0 +1,76 @@
+package com.cloudbees.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
+ */
+public class SeleniumTest {
+
+    @Test
+    public void testFirefoxDriver()  throws Exception {
+        System.err.println("");
+        System.err.println("#############################");
+        System.err.println("# INSTANTIATE FirefoxDriver");
+        System.err.println("#############################");
+
+        FirefoxDriver webDriver = new FirefoxDriver();
+        testSeleniumDriver(webDriver);
+    }
+
+    @Test
+    public void testRemoteWebDriverWithFirefox()  throws Exception {
+        List<DesiredCapabilities> capabilitiesList = Arrays.asList(new DesiredCapabilities(), DesiredCapabilities.firefox(), DesiredCapabilities.chrome(), DesiredCapabilities.htmlUnit());
+
+        for(DesiredCapabilities capabilities: capabilitiesList) {
+            try {
+                testSeleniumDriver(new RemoteWebDriver(capabilities));
+            } catch (Exception e) {
+                System.err.println("");
+                System.err.println("EXCEPTION");
+                e.printStackTrace();
+                System.err.println("");
+            }
+        }
+    }
+
+    private void testSeleniumDriver(WebDriver webDriver) {
+        System.err.println("");
+        System.err.println("#############################");
+        System.err.println("# TEST WITH " + webDriver);
+        System.err.println("#############################");
+        try {
+            webDriver.get("https://google.com");
+            System.out.println(webDriver.toString() + " -> " + webDriver.getCurrentUrl() + " -> " + webDriver.getTitle());
+        } finally {
+            webDriver.close();
+            webDriver.quit();
+        }
+    }
+}
